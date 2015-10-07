340093e814a4245c5e82228f3ec59bdbf3ffa6c1
shelbybjensen <shelbybjensen@gmail.com>
2015-10-06 20:33:58 -0600

    first

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	var color = 'white'
+	var colors = 'white red yellow green blue'
+
+	$('.box').on('click', function() {
+		$(this).addClass(color);
+	})
+	$('.box').on('dblclick', function() {
+		$(this).removeClass(colors);
+	})
+	$('#reset').on('click', function() {
+		$('.box').removeClass(colors);
+	})
+	$('#red').on('click', function() {
+		color = 'red';
+	})
+	$('#blue').on('click', function() {
+		color = 'blue';
+	})
+	$('#green').on('click', function() {
+		color = 'green';
+	})
+	$('#yellow').on('click', function() {
+		color = 'yellow';
+	})
+	$('#white').on('click', function() {
+		color = 'white';
+	})
+
+})
